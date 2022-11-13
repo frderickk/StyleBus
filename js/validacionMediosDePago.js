b4921f605947d69847e91d0f58a0d02fba88a2ab
alanfv22 <alanfveron@gmail.com>
2022-11-13 03:46:36 -0300

    medios de pago

--- js/validacionMediosDePago.js
+++ js/validacionMediosDePago.js
@@ -2,6 +2,8 @@ validartarjeta=() =>{
     
     const rtarjeta= document.getElementById("rtarjeta").value;
     const nombreYApellido= document.getElementById("tnombres").value;
+    const rnombre= document.getElementById("rnombre").value;
+    const rapellido= document.getElementById("rapellido").value;
     const fechaDeExpiracion= document.getElementById("rfecha").value;
     const cuentaMP= document.getElementById("rcuenta-mercado").value;
     const numeroDeCuentaBancaria= document.getElementById("rnumero-bancaria").value;
@@ -9,15 +11,82 @@ validartarjeta=() =>{
 
    
     const expresionesTarjeta = {
+      numeros: /[0-9]/,
+      caracterEspecial: /[!-/:-@[-`{-■]/,
+      vacio: /^$/,
+      
+       }
+
+      const expresionesNombreApellido = {
+        numeros: /[0-9]/,
+        caracterEspecial: /[ -/:-@[-`{-■]/,
         vacio: /^$/
-         }
+
+      }
 
          if (expresionesTarjeta.vacio.test(rtarjeta)){
           swal({
-              title: "El numero de tarjeta esta vacio",
+              title: "Número de tarjeta vacio",
               text: "",
               icon: "warning",
               button: "Volver a ingresar dato",
             });
       }
+
+      if (rtarjeta.length >0 && rtarjeta.length < 16){
+        swal({
+            title: "Número de tarjeta es menor a 16 digitos",
+            text: "",
+            icon: "warning",
+            button: "Volver a ingresar dato",
+          }); 
+    }
+
+    if (rtarjeta.length >16 ){
+      swal({
+          title: "Número de tarjeta excede de 16 digitos",
+          text: "",
+          icon: "warning",
+          button: "Volver a ingresar dato",
+        }); 
+  }
+
+  if (!expresionesTarjeta.numeros.test(rtarjeta)){
+    swal({
+        title: "Número de tarjeta contiene algun caracter no numerico",
+        text: "",
+        icon: "warning",
+        button: "Volver a ingresar dato",
+      });
+}
+
+
+if(  expresionesNombreApellido.numeros.test(nombreYApellido)  ){
+  swal({
+      title: "El nombre no puede contener numeros",
+      text: "",
+      icon: "warning",
+      button: "Volver a ingresar dato",
+    });
+   }
+
+   if(  expresionesNombreApellido.caracterEspecial.test(nombreYApellido)  ){
+    swal({
+        title: "El nombre no puede contener caracteres especiales",
+        text: "",
+        icon: "warning",
+        button: "Volver a ingresar dato",
+      });
+     }
+
+     if(  expresionesNombreApellido.vacio.test(nombreYApellido)  ){
+      swal({
+          title: "El nombre no puede estar vacio",
+          text: "",
+          icon: "warning",
+          button: "Volver a ingresar dato",
+        });
+       }
+
+  
 }
