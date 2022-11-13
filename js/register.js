e0304525f001e21674e59e81a914c0f70fb323d6
Agustina Insua <aguss.insua99@gmail.com>
2022-11-13 00:50:53 -0300

    asdads

--- js/register.js
+++ js/register.js
@@ -1,14 +1,37 @@
 registrar=() =>{
     
     const form= document.getElementById("form");
-    const email= document.getElementById("email").value;
-    const password= document.getElementById("contraseña").value;
+    const email= document.getElementById("remail").value;
+    const password= document.getElementById("rcontraseña").value;
     const rnombre= document.getElementById("rnombre").value;
     const rapellido= document.getElementById("rapellido").value;
     const rdomicilio= document.getElementById("rdomicilio").value;
     const rdni= document.getElementById("rdni").value;
 
-    if (rnombre == ""){
-        console.log ("Nombre esta vacio");
+    const expresionesNombres = {
+          rnombre1: /[0-9]/ ,
+          rnombre2: /^$ /
+          }
+
+    if (expresionesNombres.rnombre1.test(rnombre)){
+        swal({
+            title: "Nombre contiene numero",
+            text: "",
+            icon: "warning",
+            button: "Volver a ingresar dato",
+          }); 
     }
+
+    if (expresionesNombres.rnombre2.test(rnombre)){
+        swal({
+            title: "Nombre esta vacio",
+            text: "",
+            icon: "warning",
+            button: "Volver a ingresar dato",
+          }); 
+    }
+
+
+
 }
+
